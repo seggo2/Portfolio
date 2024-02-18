a79b678084955cd1b750d3362749ee8677b95bdc
Sefa Gür <sefa_guer@hotmail.com>
2024-02-18 13:44:24 +0100

    about me section is done

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,AboutmeComponent,ProjectsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
