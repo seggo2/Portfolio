5927bdf6353a597c9df6581c8fab6ee16fc8cd28
Sefa Gür <sefa_guer@hotmail.com>
2024-02-22 16:10:46 +0100

    finished forms area and php implemented

--- src/app/main/contact/contact.component.ts
+++ src/app/main/contact/contact.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  constructor(private http: HttpClient) {}
+
+  contentEditable = false;
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  mailTest = true;
+
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+
+  toggleEditable(event: any) {
+    if (event.target.checked) {
+      this.contentEditable = true;
+    }
+  }
 }
