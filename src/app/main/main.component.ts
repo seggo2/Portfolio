54499eca6accb05ae79d6d73d52b5f33fed89a28
Sefa Gür <sefa_guer@hotmail.com>
2024-02-22 14:26:32 +0100

    basic structure for contact formula

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,AboutmeComponent,ProjectsComponent],
+  imports: [CommonModule,AboutmeComponent,ProjectsComponent,ContactComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
