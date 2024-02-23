9f464eae362cf098e4cf18c0cd87240bc99993a7
Sefa Gür <sefa_guer@hotmail.com>
2024-02-23 23:47:09 +0100

    animations added

--- src/app/main/aboutme/aboutme.component.ts
+++ src/app/main/aboutme/aboutme.component.ts
@@ -1,24 +1,23 @@
 import { Component, HostListener , OnInit ,} from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 @Component({
   selector: 'app-aboutme',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './aboutme.component.html',
-  styleUrl: './aboutme.component.scss',
-
+  styleUrls: ['./aboutme.component.scss'], 
   animations: [
     trigger('bildAnimation', [
       state('start', style({
-        transform: 'translateX(0)',
+        transform: 'translateY(0)', 
         opacity: 1,
       })),
       transition('* => start', [
         style({
-          transform: 'translateX(100%)',
+          transform: 'translateY(100%)', 
           opacity: 0,
         }),
         animate('1000ms ease-out'),
@@ -28,12 +27,11 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
 })
 
 export class AboutmeComponent implements OnInit {
-
+  
   private isInViewport = false;
 
-  @HostListener('window:scroll', ['$event'])
-
-  animationState = 'start';
+  img = ['assets/imgs/animations/scrolldown/Group 3 (1).svg','assets/imgs/animations/scrolldown/Group 3 (2).svg','assets/imgs/animations/scrolldown/Group 3 (3).svg','assets/imgs/animations/scrolldown/Group 3 (4).svg','assets/imgs/animations/scrolldown/Group 3 (5).svg',];
+  pictureIndex = 0;
 
 
   ngOnInit(): void {
@@ -42,10 +40,11 @@ export class AboutmeComponent implements OnInit {
 
   starteAnimation() {
     setInterval(() => {
-      this.animationState = (this.animationState === 'start') ? 'void' : 'start';
-    }, 2000); 
+        this.pictureIndex = (this.pictureIndex + 1) % this.img.length;
+    }, 500); 
   }
 
+  @HostListener('window:scroll', ['$event'])
   onScroll(event: Event, className: string) {
     event.preventDefault();
     if (className !== undefined) {
@@ -63,6 +62,6 @@ export class AboutmeComponent implements OnInit {
       }
     }
   }
-  
-  
 }
+  
+
