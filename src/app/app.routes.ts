cd8970600bd7f8ee03a2e500c2778f8b5965367d
Sefa Gür <sefa_guer@hotmail.com>
2024-02-24 19:56:25 +0100

    responsive is finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ToggleMenuComponent } from './toggle-menu/toggle-menu.component';
+import { PrivacyComponent } from './rights/privacy/privacy.component';
 
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'toggle', component: ToggleMenuComponent},
+    { path: 'privacy', component: PrivacyComponent},
 ];
